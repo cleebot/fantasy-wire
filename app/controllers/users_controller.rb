053f4771a618803b234077a72806419f4b464ca6
Seth Lee <sethlee@seths-air.attlocal.net>
2021-08-25 15:46:52 -0400

    rails progress

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,9 +16,9 @@ class UsersController < ApplicationController
   # POST /users
   def create
     @user = User.new(user_params)
-    
+
     if @user.save
-      @token = encode({id: @user.id})
+      @token = encode({ id: @user.id })
       render json: {
         user: @user.attributes.except("password_digest"),
         token: @token
@@ -50,6 +50,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:username, :email, :password )
+      params.require(:user).permit(:username, :email, :password)
     end
 end
