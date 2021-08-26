b6c89d3cc168909fc99cf184024454a7b357300f
Seth Lee <sethlee@seths-air.attlocal.net>
2021-08-26 19:05:24 -0400

    building structure

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Browser>
     <App />
+    </Browser>
   </React.StrictMode>,
   document.getElementById('root')
 );
