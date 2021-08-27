5a9646de99e7797094f9e4943ba4267228238708
Seth Lee <sethlee@seths-air.attlocal.net>
2021-08-27 13:58:22 -0400

    cleaning up

--- client/src/index.js
+++ client/src/index.js
@@ -6,14 +6,11 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Browser>
+    <Router>
     <App />
-    </Browser>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
