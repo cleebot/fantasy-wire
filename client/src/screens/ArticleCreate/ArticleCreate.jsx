8810327d1c7c9c2105766fd5353b984e09bd4067
Seth Lee <sethlee@seths-air.attlocal.net>
2021-08-31 12:21:15 -0400

    create article css

--- client/src/screens/ArticleCreate/ArticleCreate.jsx
+++ client/src/screens/ArticleCreate/ArticleCreate.jsx
@@ -20,24 +20,41 @@ export default function ArticleCreate(props) {
   };
 
   return (
+    <div class="bg-grey-lighter min-h-screen flex flex-col">
+      <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+      <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
     <form
       onSubmit={(e) => {
         e.preventDefault();
         handleCreate(formData);
       }}
     >
-      <h3>Create Article</h3>
+      <h3 class="mb-8 text-3xl text-center">Create Article</h3>
       <label>
         Title:
-        <input type='text' name='title' value={title} onChange={handleChange} />
+        <input
+        type='text'
+        class="block border border-grey-light w-full p-3 rounded mb-4"
+        name='title'
+        value={title}
+        onChange={handleChange} />
       </label>
       <label>
         Content:
-        <input type='text' name='content' value={content} onChange={handleChange} />
+        <input
+        type='text'
+        name='content'
+        class="block border border-grey-light w-full p-3 rounded mb-4"
+        value={content}
+        onChange={handleChange} />
       </label>
       <label>
         Image:
-        <input type='text' name='img_url' value={img_url} onChange={handleChange} />
+        <input type='text'
+        class="block border border-grey-light w-full p-3 rounded mb-4"
+        name='img_url'
+        value={img_url}
+        onChange={handleChange} />
       </label>
       <select onChange={handleChange} name="category_id" value={category_id}>
             <option value="category">Category</option>
@@ -47,8 +64,12 @@ export default function ArticleCreate(props) {
               )
             })}
           </select>
-      <button>Submit</button>
-    </form>
+      <button type="submit"
+      class="w-full text-center py-3 rounded bg-green text-black hover:bg-green-dark focus:outline-none my-1">Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
 
