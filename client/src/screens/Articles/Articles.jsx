b9f088ff479f3e9432e3c600f5ec2d4ecf31ff48
Seth Lee <sethlee@seths-air.attlocal.net>
2021-08-31 15:20:56 -0400

    articles cards good

--- client/src/screens/Articles/Articles.jsx
+++ client/src/screens/Articles/Articles.jsx
@@ -11,27 +11,29 @@ export default function Articles(props) {
         <button>Create an Article!</button>
       </Link>
       <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
-      {articles.map((article) => (
+        {articles.map((article) => (
+        <div className="w-80 bg-white rounded-xl overflow-hidden shadow-lg hover:shadow-xl hover:scale-105 duration-500 transform transition cursor-pointer">
         <div key={article.id}>
           <Link to={`/articles/${article.id}`}>
           <img className="mx-auto" src={`${article.img_url}`}
       alt={`${article.name}`}
         />
-            <p>{article.title}</p>
+            <p className="text-1xl font-bold text-gray-600">{article.title}</p>
           </Link>   
             <div>
               <Link to={`/articles/${article.id}/edit`}>
-                <button className="mr-4">Edit</button>
+                <button className="mr-4 text-gray-600">Edit</button>
               </Link>
-              <button onClick={() => handleDelete(article.id)}>Delete</button>
+              <button className="text-gray-600" onClick={() => handleDelete(article.id)}>Delete</button>
           </div>
-        </div>
+            </div>
+            </div>
       )
         )}
+        </div>
       {/* <Link to='/articles/new'>
         <button>Create</button>
       </Link> */}
-    </div>
     </div>
   )
 }
